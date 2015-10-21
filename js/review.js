41f4f50463e7fdfafc6e11b7601c9e2e07e560dc
Alex Troupe <adtroupe@uw.edu>
2015-10-21 15:13:09 -0700

    pre-raty update

--- js/review.js
+++ js/review.js
@@ -1,12 +1,16 @@
+$('#rating').raty();
+
+// Parse.initialize("m7CnuhWD7dIkJvze4ynK1BBbxH7D0JjGgfn9Iln1", "5Tm7XUvanrHDWsUKnuH5Ac07SrgmDKkeP714Xg7y");
+
+// var ProductReviews = Parse.Object.extend('ProductReviews');
 
 
-parse.initialize("m7CnuhWD7dIkJvze4ynK1BBbxH7D0JjGgfn9Iln1", "5Tm7XUvanrHDWsUKnuH5Ac07SrgmDKkeP714Xg7y");
 
-var ProductReviews = parse.object.extend('ProductReviews');
+// $('form').submit(function(){
+// 	var review = new ProductReviews();
 
-$('form').submit(function(){
-	var review = new ProductReviews();
+// 	$(this).find('input').each()
 
 
-});
+// });
 
