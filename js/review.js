34ecf54b087da5de007774831c58db78893ab574
Alex Troupe <adtroupe@uw.edu>
2015-10-21 17:44:43 -0700

    working order, no visable reviews yet

--- js/review.js
+++ js/review.js
@@ -1,16 +1,75 @@
-$('#rating').raty();
+$(document).ready(function() {
 
-// Parse.initialize("m7CnuhWD7dIkJvze4ynK1BBbxH7D0JjGgfn9Iln1", "5Tm7XUvanrHDWsUKnuH5Ac07SrgmDKkeP714Xg7y");
+	Parse.initialize("m7CnuhWD7dIkJvze4ynK1BBbxH7D0JjGgfn9Iln1", "5Tm7XUvanrHDWsUKnuH5Ac07SrgmDKkeP714Xg7y");
 
-// var ProductReviews = Parse.Object.extend('ProductReviews');
+	var ProductReviews = Parse.Object.extend('ProductReviews');
+
+	$('#avgRating').raty({
+		half: true
+	});
+
+	$('#userRating').raty({
+		half: true
+	});
+
+	$('form').submit( function() {
+		var review = new ProductReviews();
+		review.set({
+			userRating: parseFloat($('#userRating>input').val()),
+			name: $('#name').val(),
+			title: $('#title').val(),
+			userReview: $('#review').val(),
+			upVotes: 0,
+			downVotes: 0
+		});
+		$('#userRating>input').val('0');
+		$('#name').val('');
+		$('#title').val('');
+		$('#review').val('');
+
+		review.save(null, {
+			success:function(){
+				getData();
+			}
+		});
+		return false;
+	});
 
 
 
-// $('form').submit(function(){
-// 	var review = new ProductReviews();
+	var getData = function() {
+		var query = new Parse.Query(ProductReviews);
+		query.find({
+			success: function(results) {
+				buildReviews(results);
+			}
+		});
+	};
 
-// 	$(this).find('input').each()
 
 
-// });
+	var buildReviews = function(data) {
+		$('#reviewArea').empty();
+		data.forEach(function(d) {
+			addReview(d);
+		});
+	};
 
+
+	var addReview = function(review) {
+		var userRating = review.get('userRating');
+		var user = review.get('name');
+		var reviewDate = review.get('createdAt');
+		var title = review.get('title');
+		var userReview = review.get('userReview');
+		var upVotes = review.get('upVotes');
+		var downVotes = review.get('downVotes');
+
+		console.log(userRating+user+reviewDate+title+userReview+upVotes+downVotes);
+
+
+	};
+
+
+	getData();
+});
